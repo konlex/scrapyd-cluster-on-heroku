eb5c0cb77c7f4864f0fc097e6a27d58ee18c5ad0
konlex <50760537+konlex@users.noreply.github.com>
2020-05-07 15:36:45 +0800

    Update scrapydweb_settings_v10.py

--- scrapydweb/scrapydweb_settings_v10.py
+++ scrapydweb/scrapydweb_settings_v10.py
@@ -27,8 +27,8 @@ ENABLE_AUTH = True
 if os.environ.get('ENABLE_AUTH', 'False') == 'True':
     ENABLE_AUTH = True
 # In order to enable basic auth, both USERNAME and PASSWORD should be non-empty strings.
-USERNAME = 'konlex'
-PASSWORD = 'ggkhk99!Q'
+USERNAME = ''
+PASSWORD = ''
 USERNAME = os.environ.get('USERNAME', 'admin')
 PASSWORD = os.environ.get('PASSWORD', 'scrapydweb')
 
